e0665165209446a7dc0a9cc580aa020b18e61f25
tanya22222222554646543469 <147590403+tanya22222222554646543469@users.noreply.github.com>
2024-03-29 13:50:45 +0700

    добавление тесов

--- tests/CollectTest.php
+++ tests/CollectTest.php
@@ -10,18 +10,17 @@ class CollectTest extends TestCase
         $this->assertSame(2, $collect->count());
     }
 
+    public function testKeys()
+    {
+        $collect = new Collect\Collect(['a' => 1, 'b' => 2]);
+        $this->assertSame(['a', 'b'], $collect->keys()->toArray());
+    }
 
     public function testUnshift()
     {
-        $collect = $this->getMockBuilder(Collect\Collect::class)
-            ->onlyMethods(['unshift'])
-            ->getMock();
-
-        $collect->expects($this->once())
-            ->method('unshift')
-            ->with([0, -1]);
-
-        $collect->unshift([0, -1]);
+        $collect = new Collect\Collect([2, 3]);
+        $collect->unshift(1);
+        $this->assertSame([1, 2, 3], $collect->toArray());
     }
 
 
@@ -33,12 +32,17 @@ class CollectTest extends TestCase
         $this->assertEquals([2, 3, 4], $result->toArray());
     }
 
+    public function testValues()
+    {
+        $collect = new Collect\Collect(['a' => 1, 'b' => 2]);
+        $this->assertSame([1, 2], $collect->values()->toArray());
+    }
+
     public function testPush()
     {
-        $collect = new Collect\Collect([1, 2, 3]);
-        $result = $collect->push(4);
-        $this->assertInstanceOf(Collect\Collect::class, $result);
-        $this->assertEquals([1, 2, 3, 4], $result->toArray());
+        $collect = new Collect\Collect(['a' => 1, 'b' => 2, 'c' => 3]);
+        $collect->push(4, 'd');
+        $this->assertSame(['a' => 1, 'b' => 2, 'c' => 3, 'd' => 4], $collect->toArray());
     }
 
 
@@ -50,21 +54,6 @@ class CollectTest extends TestCase
         $this->assertEquals(['a' => 1, 'c' => 3], $result->toArray());
     }
 
-    public function testKeys()
-    {
-        $collect = new Collect\Collect(['a' => 1, 'b' => 2, 'c' => 3]);
-        $result = $collect->keys();
-        $this->assertInstanceOf(Collect\Collect::class, $result);
-        $this->assertEquals(['a', 'b', 'c'], $result->toArray());
-    }
-
-    public function testValues()
-    {
-        $collect = new Collect\Collect(['a' => 1, 'b' => 2, 'c' => 3]);
-        $result = $collect->values();
-        $this->assertInstanceOf(Collect\Collect::class, $result);
-        $this->assertEquals([1, 2, 3], $result->toArray());
-    }
 
     public function testToArray()
     {
@@ -75,28 +64,20 @@ class CollectTest extends TestCase
     public function testPop()
     {
         $collect = new Collect\Collect([1, 2, 3]);
-
-        $result = $collect->pop();
-
-        $this->assertInstanceOf(Collect\Collect::class, $result);
-
-        $this->assertEquals([1, 2], $result->toArray());
+        $collect->pop();
+        $this->assertSame([1, 2], $collect->toArray());
     }
-    // public function testSplice()
-    // {
-    //     // Создаем объект Collect с начальными данными [1, 2, 3, 4, 5]
-    //     $collect = new Collect\Collect([1, 2, 3, 4, 5]);
 
-    //     // Копируем начальное состояние коллекции
-    //     $initialArray = $collect->toArray();
-
-    //     // Вызываем метод splice() с аргументом 1 и 2
-    //     $collect->splice(1, 2);
-
-    //     // Проверяем, что метод splice() изменяет данные в коллекции
-    //     $this->assertEquals([1, 4, 5], $collect->toArray());
-    // }
-
+    public function testSearch()
+    {
+        $collect = new Collect\Collect([
+            ['id' => 1, 'name' => 'Luca'],
+            ['id' => 2, 'name' => 'Luca'],
+            ['id' => 3, 'name' => 'Tobi']
+        ]);
+        $result = $collect->search('name', 'Luca')->toArray();
+        $this->assertSame([['id' => 1, 'name' => 'Luca'], ['id' => 2, 'name' => 'Luca']], $result);
+    }
 
     public function testExcept()
     {
